5063ad3ceb3830d10a5679d54d3579db5644c0f9
KirillArturov <curillaenator@gmail.com>
2025-05-03 09:58:00 +0700

    fix: clean

--- src/entities/user/constants.ts
+++ src/entities/user/constants.ts
@@ -1,4 +1,4 @@
-import type { AppBotname } from '@src/providers/AppBotnameProvider';
+import type { AppBotname } from '@src/app';
 
 const BOTNAME_TO_OWNER_UID: Record<AppBotname, string> = {
   divebot: 'zRjxS4A4egRmzoHvSxM2C3jEE6W2', // Vik
